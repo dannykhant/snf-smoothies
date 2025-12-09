8ec3613b9c0d59f0de875b6c009a3dad6d1e6bca
Danny Khant <dannypmkhant@gmail.com>
2025-12-09 16:32:02 +0700

    feat: fetch and display nutrition information for ingredients

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,9 @@ if ingredients_list:
     
     for x in ingredients_list:
         ingredients_string += x + " "
+        st.subheader(x + " Nutrition Information")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
 
@@ -46,6 +49,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
